85aa3864bc4c74011d6b618a284d4fc820320658
trizzi <patrickakhamiogu@gmail.comdquote>
2022-01-07 21:41:31 +0100

    uploading navbar prototype

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello!!
-    </h1>
+    <Router>
+      <>
+        <Navbar />
+      </>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Button from './button-component/Button';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav
+      className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-
+      mulish'
+      role='navigation'>
+      <Link to='#' className='pl-2'>
+        <img
+          className='w-25 h-20 p-2'
+          src='/img/logo.svg'
+          alt='Logo'
+        />
+      </Link>
+      <div className='px-8 cursor-pointer md:hidden'>
+        <i className='fas fa-bars'></i>
+      </div>
+      <div className='pr-8 md:block hidden'>
+        <Link className='p-4' to='#'>
+          FAQs
+        </Link>
+        <Link to='#' className='p-4'>
+          CONTACT US
+        </Link>
+        <Link to='#' className='p-4'>
+          BLOG
+        </Link>
+        <Link to='#' className='p-4'>
+          ABOUT
+        </Link>
+        <Button
+          className='p-4 border bg-orange-600 border-orange-600 hover:border-orange-400 ...'
+          text='GET STARTED'
+        />
+
+        <Button
+          className='p-4 border border-orange-600 hover:border-orange-400 ...  '
+          text='LOGIN'
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
