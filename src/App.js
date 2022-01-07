d2d89c90826ca915abf4256045ec7bd83e4f4e55
trizzi <patrickakhamiogu@gmail.comdquote>
2022-01-07 12:47:14 +0100

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <div>Hello!!</div>;
+  return (
+    <h1 className='text-3xl font-bold underline'>
+      Hello!!
+    </h1>
+  );
 }
 
 export default App;
