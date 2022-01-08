b81cae73607ebe2ab9fdceda4b58aeacc7dd1c4e
itstimiking <26443032+itstimiking@users.noreply.github.com>
2022-01-08 18:12:47 +0100

    Footer section added

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import AppDownload from './components/AppDownload';
 import Blog from './components/blog-section/Blog';
 import CustomerReview from './components/customer-review/CustomerReview';
+import Footer from './components/footer/Footer';
 import Navbar from './components/Navbar';
 import Services from './components/services/Services';
 import Showcase from './components/Showcase';
+import { ThemeContextProvider } from './context/ThemeContext';
+
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Showcase />
-        <Services />
-        <AppDownload />
-        <CustomerReview />
-        <Blog />
-      </>
-    </Router>
+    <ThemeContextProvider>
+      <Router>
+        <>
+          <Navbar />
+          <Showcase />
+          <Services />
+          <AppDownload />
+          <CustomerReview />
+          <Blog />
+          <Footer />
+        </>
+      </Router>
+    </ThemeContextProvider>
   );
 }
 
