79a404348cbe480d2de18dd7205a0cf28a7daf16
trizzi <patrickakhamiogu@gmail.comdquote>
2022-01-12 14:34:28 +0100

    Uploading the prototype for the signin and signup components

--- src/components/registration/Signin.js
+++ src/components/registration/Signin.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import Button from '../button-component/Button';
 
 const Signin = () => {
   return (
     <div>
-      <h1>Signin</h1>
+      <div className='left'>
+        <div>
+          <img src='' alt='' />
+        </div>
+        <p>
+          ShopRacks, as a business solution leverages on
+          technology to facilitate the core aspect of your
+          online store. We provides a platform for you to
+          easily document, send and track invoices. You can
+          also manage and track inventories, orders and
+          deliveries seemlessly
+        </p>
+        <p>Shopracks, Nigeria</p>
+        <p>OTHER FINANCE PRODUCTS</p>
+        <div>
+          <p>Shopracks Subscriptions</p>
+          <p>Shopracks Expenses</p>
+        </div>
+      </div>
+      <div className='right'>
+        <img src='' alt='' />
+        <form action=''>
+          <input type='text' placeholder='Email Address' />
+          <input type='text' placeholder='Password' />
+          <Button text='SIGN IN' background />
+          <p>
+            Already have a Shopracks account?
+            <span>Sign Up</span>
+          </p>
+          <p>Forgot Password??</p>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/registration/Signup.js
+++ src/components/registration/Signup.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import Button from '../button-component/Button';
 
 const Signup = () => {
   return (
     <div>
-      <h1>Signup</h1>
+      <div className='left'>
+        <div>
+          <img src='' alt='' />
+        </div>
+        <p>
+          ShopRacks, as a business solution leverages on
+          technology to facilitate the core aspect of your
+          online store. We provides a platform for you to
+          easily document, send and track invoices. You can
+          also manage and track inventories, orders and
+          deliveries seemlessly
+        </p>
+        <p>Shopracks, Nigeria</p>
+        <p>OTHER FINANCE PRODUCTS</p>
+        <div>
+          <p>Shopracks Subscriptions</p>
+          <p>Shopracks Expenses</p>
+        </div>
+      </div>
+      <div className='right'>
+        <img src='' alt='' />
+        <form action=''>
+          <input type='text' placeholder='Company Name' />
+          <input type='text' placeholder='Email Address' />
+          <input type='text' placeholder='Password' />
+          <input type='text' placeholder='Phone Number' />
+          <input type='checkbox' />
+          <Button text='SIGN UP' background />
+          <p>
+            Already have a Shopracks account?
+            <span>Sign In</span>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
