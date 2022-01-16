b2f56fd151a0d2a9b3c4ed455f21879af54c4e38
itstimiking <26443032+itstimiking@users.noreply.github.com>
2022-01-16 20:35:18 +0100

    update animation

--- src/routes/registration/Signin.js
+++ src/routes/registration/Signin.js
@@ -1,4 +1,4 @@
-import React,{ useEffect, useRef} from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 import Button from '../../components/button-component/Button';
 import { useTheme } from '../../context/ThemeContext';
 import { Link } from 'react-router-dom';
